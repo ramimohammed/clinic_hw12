65fae46676f968f8bfad9686b9aeb816df0bb60d
Rami <ramia872000@gmail.com>
2017-04-26 20:18:56 +0000

    add req validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,16 @@
 class Doctor < ActiveRecord::Base
      has_many :patients, dependent: :destroy
+     
+     
+     
+      validates :first_name, :presence => true   
+    
+      validates :last_name, :presence => true
+      validates_uniqueness_of :license_number
+     validates_uniqueness_of :email
+    validates_uniqueness_of :phone_number
+    validates_format_of :phone_number, with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/,:message => "- must be like 123-456-7890"
+    validates_format_of :email, :with => /\A([^@\s]+)@((gmail)\.+com)\Z/i, :on => :create ,:message =>"must be like  name@gmail.com"  
+
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,19 @@
 class Patient < ActiveRecord::Base
   belongs_to :doctor
+  
+  
+  validates_presence_of :first_name, message: 'please fill the blank'
+  validates_presence_of :last_name, message: 'please fill the blank'
+  validates_uniqueness_of :first_name
+  validates_uniqueness_of :last_name
+  
+  validates_uniqueness_of :phone
+  validates_format_of :phone,:with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,:message => "- Phone number should be like  123-456-7890"
+  
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((gmail)\.+com)\Z/i, :on => :create,:message =>"must like name@gmail.com"
+    
+  
+
+  
 end
